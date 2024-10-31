fd3ac53b7462f2875eea8606880ff0bc6a429f45
rev <108827530+whyblurry@users.noreply.github.com>
2024-10-31 13:55:47 -0400

    update

---
+++ css/js/script.js
@@ -0,0 +1,1 @@
+function h(e) { e.preventDefault(), window.removeEventListener("touchstart", h, null), window.removeEventListener("click", h, null); const t = new Audio("music/temp.mp3"); document.querySelector(".before").remove(), document.querySelector(".after").style.display = "block", t.play() } window.addEventListener("touchstart", h), window.addEventListener("click", h);
